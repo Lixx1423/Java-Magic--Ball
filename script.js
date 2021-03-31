5aad2af24aaa743b7989d6a2749170bdda4254b6
Kohn_Charles <Kohn_Charles@student.mahoningctc.com>
2021-03-31 13:57:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+function MagicResponse(){
+var firstInput = "userQuestion"
+userQuestion = document.getElementById("firstInput").value;
+document.getElementById("output").innerHTML = userQuestion;
+
+Math.floor(Math.random() * 10) + 1;
+
+switch(Answer){
+case 1:
+    answer = "It is certain"
+    document.getElementById("8ball").value;
+    break
+case 2:
+    answer = "Yes - definitely"
+    document.getElementById("8ball").value;
+    break
+case 3:
+    answer = "As I see it, yes"
+    document.getElementById("8ball").value;
+    break
+case 4:
+    answer = "Cannot predict now"
+    document.getElementById("8ball").value;
+    break
+case 5:
+    answer = "Ask again later"
+    document.getElementById("8ball").value;
+    break
+case 6:
+    answer = "Better not tell you now"
+    document.getElementById("8ball").value;
+    break
+case 7:
+    answer = "Don't count on it"
+    document.getElementById("8ball").value;
+    break
+case 8:
+    answer = "My sources say no"
+    document.getElementById("8ball").value;
+    break
+case 9:
+    answer = "My reply is no"
+    document.getElementById("8ball").value;
+    break
+case 10:
+    answer = "Very doubtful"
+    document.getElementById("8ball").value;
+    break
+
+
+
+
+
+
+
+
+
+
+
+}
+
+}
